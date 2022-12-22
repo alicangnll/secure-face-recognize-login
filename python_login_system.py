1bb4d60210ba07a771e0e66bba6702d618c1f5d0
Ali Can Gönüllü <alicangonullu@yahoo.com>
2022-12-22 23:12:19 +0300

    Secure Login System with Python
    Secure Login System with Python

--- python_login_system.py
+++ python_login_system.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 import base64
 
 app = Flask(__name__)
+secret_key = "alicangonullu" # Secret key for decryption
 
 @app.route('/')
 def main():
@@ -13,7 +14,6 @@ def main():
 def face_login():
     image = str(request.form.get("current_image")) #Base64 image from webcam
     email = str(request.form.get("email")) # Email address from database
-    secret_key = "alicangonullu" # Secret key for decryption
     
     fh = open("temp.png", "wb")
     fh.write(base64.decodebytes(image.encode('utf-8'))) #Write your temp face file to temp file
@@ -52,7 +52,7 @@ def savefile():
     fh.write(base64.decodebytes(image.encode('utf-8')))# Save your photo
     fh.close()
 
-    pyAesCrypt.encryptFile("loginlist/" + email + ".jpg", "loginlist/" + email + ".crypt", "alicangonullu") # Decrypt your verification photo
+    pyAesCrypt.encryptFile("loginlist/" + email + ".jpg", "loginlist/" + email + ".crypt", secret_key) # Decrypt your verification photo
     os.remove("loginlist/" + email + ".jpg") # Delete photo
     return "OK"
 
